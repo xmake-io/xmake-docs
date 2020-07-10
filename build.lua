1e70d9606394e5464d6a28a2bf85e0d75d63bac4
ruki <waruqi@gmail.com>
2020-07-10 23:13:40 +0800

    build mirror files

--- build.lua
+++ build.lua
@@ -1,9 +1,14 @@
--- generate sitemap.xml
 function mtime(file)
     return os.date("%Y-%m-%dT%H:%M:%S+08:00", os.mtime(file))
 end
+
+-- generate mirror files and sitemap.xml
+-- we need install https://github.com/cwjohan/markdown-to-html first
+-- npm install markdown-to-html -g
+--
 function main()
     local siteroot = "https://xmake.io"
+    local mirrordir = "mirror"
     local sitemap = io.open("sitemap.xml", 'w')
     sitemap:print([[
 <?xml version="1.0" encoding="UTF-8"?>
@@ -19,25 +24,39 @@ function main()
   <lastmod>%s</lastmod>
 </url>
 ]], siteroot, mtime("index.html"))
+    os.rm(mirrordir)
     for _, markdown in ipairs(os.files("**.md")) do
         local basename = path.basename(markdown)
         if not basename:startswith("_") then
+
+            -- get the raw url
             if basename == "README" then
                 basename = ""
             end
-            local url = siteroot .. '/#'
+            local url = siteroot .. '/mirror'
+            local rawurl = siteroot .. '/#'
             local dir = path.directory(markdown)
             if dir ~= '.' then
+                rawurl = rawurl .. '/' .. dir
                 url = url .. '/' .. dir
             end
-            url = url .. '/' .. basename
-            print("build %s => %s, %s", markdown, url, mtime(markdown))
+            rawurl = rawurl .. '/' .. basename
+            url = url .. '/' .. (basename == "" and "index.html" or (basename .. ".html"))
+
+            -- generate html file
+            local htmlfile = path.join(mirrordir, dir, basename == "" and "index.html" or (basename .. ".html"))
+            local htmldata = os.iorunv("markdown", {markdown})
+            io.writefile(htmlfile, htmldata)
+            print("build %s => %s, %s", markdown, htmlfile, mtime(htmlfile))
+            print("url %s -> %s", url, rawurl)
+
+            -- generate sitemap
             sitemap:print([[
 <url>
   <loc>%s</loc>
   <lastmod>%s</lastmod>
 </url>
-]], url, mtime(markdown))
+]], url, mtime(htmlfile))
         end
     end
     sitemap:print("</urlset>")
