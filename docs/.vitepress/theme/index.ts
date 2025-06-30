b3d6586ad39f2cf5b0bc31529b8d3e07dd931eb2
ruki <waruqi@gmail.com>
2025-06-30 23:55:50 +0800

    update route

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,9 +3,15 @@ import './styles.css'
 import 'virtual:group-icons.css'
 
 const hashToRoute = {
+  '#/guide/quickstart': '/guide/quick-start',
+  '#/guide/installation': '/guide/quick-start#installation',
+  '#/guide/configuration': '/guide/basic-commands/build-configuration',
   '#/about/sponsor': '/about/sponsor',
   '#/manual/project_target': '/api/description/project-target',
   '#/manual/project_target?id=phony': '/api/description/project-target#phony',
+  '#/zh-cn/guide/quickstart': '/zh/guide/quick-start',
+  '#/zh-cn/guide/installation': '/zh/guide/quick-start#installation',
+  '#/zh-cn/guide/configuration': '/zh/guide/basic-commands/build-configuration',
   '#/zh-cn/manual/project_target': '/zh/api/description/project-target',
   '#/zh-cn/manual/project_target?id=phony': '/zh/api/description/project-target#phony',
   '#/zh-cn/about/sponsor': '/zh/about/sponsor',
