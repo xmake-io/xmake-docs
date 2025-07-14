9454da398d2ac8de8ddc422d5e95047af4f4b1a5
ruki <waruqi@gmail.com>
2025-07-14 23:22:24 +0800

    fix npm

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,7 @@
 npm add vitepress@2.0.0-alpha.6 -D --save-dev
 npm add vitepress-plugin-group-icons -D
 npm add vitepress-plugin-llms -D
+npm add gray-matter -D
+npm add markdown-it -D
 npm install
 vitepress build docs
