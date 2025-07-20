138bddfc6b1c968fa9162aadaee685238c49979b
ruki <waruqi@gmail.com>
2025-07-20 21:51:08 +0800

    improve netlify

--- netlify/edge-functions/language-redirect.js
+++
@@ -1,9 +0,0 @@
-export default async (request, context) => {
-  if (request.url.pathname === "/") {
-    const lang = request.headers.get("accept-language") || "";
-    if (lang.startsWith("zh")) {
-      return Response.redirect("/zh/", 302);
-    }
-  }
-  return context.next();
-};
