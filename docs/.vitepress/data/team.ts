cafe0e8a560438625060c4b071d5eafb486eb435
ruki <waruqi@gmail.com>
2026-01-16 22:46:13 +0800

    add members

--- docs/.vitepress/data/team.ts
+++ docs/.vitepress/data/team.ts
@@ -24,19 +24,96 @@ export const coreTeamMembers: DefaultTheme.TeamMember[] = [
     links: [
       { icon: 'github', link: 'https://github.com/star-hengxing' }
     ]
+  },
+  {
+    avatar: 'https://github.com/SirLynix.png',
+    name: 'SirLynix',
+    title: 'Maintainer',
+    org: 'Xmake',
+    orgLink: 'https://xmake.io',
+    desc: 'Maintains <a href="https://github.com/xmake-io/xmake-repo" target="_blank">xmake-repo</a> repository',
+    links: [
+      { icon: 'github', link: 'https://github.com/SirLynix' }
+    ]
+  },
+  {
+    avatar: 'https://github.com/apocelipes.png',
+    name: 'apocelipes',
+    title: 'Maintainer',
+    org: 'Xmake',
+    orgLink: 'https://xmake.io',
+    desc: 'Maintains <a href="https://github.com/xmake-io/xmake-repo" target="_blank">xmake-repo</a> repository',
+    links: [
+      { icon: 'github', link: 'https://github.com/apocelipes' }
+    ]
+  },
+  {
+    avatar: 'https://github.com/xq114.png',
+    name: 'xq114',
+    title: 'Maintainer',
+    org: 'Xmake',
+    orgLink: 'https://xmake.io',
+    desc: 'Maintains <a href="https://github.com/xmake-io/xmake-repo" target="_blank">xmake-repo</a> repository',
+    links: [
+      { icon: 'github', link: 'https://github.com/xq114' }
+    ]
+  },
+  {
+    avatar: 'https://github.com/luadebug.png',
+    name: 'luadebug',
+    title: 'Maintainer',
+    org: 'Xmake',
+    orgLink: 'https://xmake.io',
+    desc: 'Maintains <a href="https://github.com/xmake-io/xmake-repo" target="_blank">xmake-repo</a> repository',
+    links: [
+      { icon: 'github', link: 'https://github.com/luadebug' }
+    ]
+  },
+  {
+    avatar: 'https://github.com/Arthapz.png',
+    name: 'Arthapz',
+    title: 'Maintainer',
+    org: 'Xmake',
+    orgLink: 'https://xmake.io',
+    desc: 'Maintains C++ Modules support',
+    links: [
+      { icon: 'github', link: 'https://github.com/Arthapz' }
+    ]
   }
 ]
 
 export const coreTeamEmeriti: DefaultTheme.TeamMember[] = [
   {
-    avatar: 'https://github.com/octocat.png',
-    name: 'Octocat',
-    title: 'Contributor',
-    org: 'GitHub',
-    orgLink: 'https://github.com',
-    desc: 'An honorary member for testing layout.',
+    avatar: 'https://github.com/da-liii.png',
+    name: 'da-liii',
+    title: 'Maintainer',
+    org: 'Xmake',
+    orgLink: 'https://xmake.io',
+    desc: 'Maintains <a href="https://github.com/xmake-io/xmake-repo" target="_blank">xmake-repo</a> repository',
     links: [
-      { icon: 'github', link: 'https://github.com/octocat' }
+      { icon: 'github', link: 'https://github.com/da-liii' }
+    ]
+  },
+  {
+    avatar: 'https://github.com/c8ef.png',
+    name: 'c8ef',
+    title: 'Maintainer',
+    org: 'Xmake',
+    orgLink: 'https://xmake.io',
+    desc: 'Maintains <a href="https://github.com/xmake-io/xmake-repo" target="_blank">xmake-repo</a> repository',
+    links: [
+      { icon: 'github', link: 'https://github.com/c8ef' }
+    ]
+  },
+  {
+    avatar: 'https://github.com/OpportunityLiu.png',
+    name: 'OpportunityLiu',
+    title: 'Contributor',
+    org: 'Xmake',
+    orgLink: 'https://xmake.io',
+    desc: 'Contributed to CUDA and vsxmake plugins',
+    links: [
+      { icon: 'github', link: 'https://github.com/OpportunityLiu' }
     ]
   }
 ]
